06bea2da71715c9f852f85c6422e7c6da3af02db
Ckrivko <76474570+Ckrivko@users.noreply.github.com>
2021-03-01 11:47:49 +0200

    Add files via upload

---
+++ Animal.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class Animal
+    {
+        private string name;
+        private int age;
+        private string gender;
+
+        public Animal(string name, int age, string gender)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Gender = gender;
+           
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Invalid input!");
+                               
+                }
+                else
+                {
+                    name = value;
+                }
+            
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                if (value<=0)
+                {
+
+                    throw new ArgumentException("Invalid input!");
+                
+                }
+                else 
+                {
+                    age = value;
+                }
+            
+            }
+        }
+        public string Gender
+        {
+            get
+            {
+                return gender;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Invalid input!");
+                }
+                else 
+                {
+                    gender = value;
+                }
+                        
+            }
+
+        }
+        //public virtual string ProduceSound() 
+        //{
+        //    return string.Empty;
+        //}
+
+        public virtual string ProduceSound() // тук си разцъквам нещо мое
+        {
+            return string.Empty;
+        }
+
+        public override string ToString()
+        {
+            return this.GetType().Name +
+                   Environment.NewLine +
+            $"{this.Name} {this.Age} {this.Gender}";
+                       
+        }
+    }
+}
---
+++ StartUp.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Animals
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            List<Animal> animals = new List<Animal>();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "Beast!")
+                {
+                    break;
+                }
+
+                string[] secondLine = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                try
+                {
+                    animals = GetAnimal(animals, secondLine, input);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+            }
+
+            foreach (var animal in animals)
+            {
+                Console.WriteLine(animal);
+                animal.ProduceSound();
+            }
+
+
+        }
+
+        public static List<Animal> GetAnimal(List<Animal> animals, string[] secondInput, string type)
+        {
+
+            if (type != "Dog" && type != "Frog" && type != "Cat" && type != "Tomcat" && type != "Kitten")
+            {
+                throw new ArgumentException("Invalid input!");
+            }
+
+
+            string name = secondInput[0];
+            int age = int.Parse(secondInput[1]);
+            string gender = secondInput[2];
+
+            if (type == "Dog")
+            {
+                Dog dog = new Dog(name, age, gender);
+                animals.Add(dog);
+            }
+
+            else if (type == "Frog")
+            {
+                Frog frog = new Frog(name, age, gender);
+                animals.Add(frog);
+            }
+
+            else if (type == "Cat")
+            {
+                Cat cat = new Cat(name, age, gender);
+                animals.Add(cat);
+
+            }
+
+
+
+            else if (type == "Tomcat")
+            {
+                Tomcat tomcat = new Tomcat(name, age);
+                animals.Add(tomcat);
+            }
+
+
+            else if (type == "Kitten")
+            {
+                Kitten kitten = new Kitten(name, age);
+                animals.Add(kitten);
+            }
+
+
+
+
+
+
+
+            return animals;
+
+        }
+
+    }
+}
